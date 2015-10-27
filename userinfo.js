54b4edefc44f24b5291318a2fabae33b5f199404
Jimit Patel <jdp59@njit.edu>
2015-10-27 15:46:34 -0400

    Questions Array Modified

--- userinfo.js
+++ userinfo.js
@@ -20,61 +20,61 @@ var start = [{
 }];
 
 inquirer.prompt( start, function( answers ) {
+
     var outputFilename = 'engineers/' + answers.name.replace(/\s/gi, "_") + ".json";
+
     fs.exists(outputFilename, function(exists) {
-        var questions = [];
-        var prev_phone = " ";
-        questions.push(
 
-            {
-                type: "input",
-                name: "gravatar",
-                message: "Please enter email address for gravatar link :-",
-                validate: function( value ) {
-                    var pass = value.match(/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/);
-                    if (pass) {
-                        return true;
-                    } else {
-                        return "Please enter link";
-                    }
-                },
-                filter:function(val){
-                    return "https://s.gravatar.com/avatar/" + MD5(val.trim().toLowerCase());
-                }
-            },
-            {
-                type: "input",
-                name: "twitter",
-                message: "Please enter your twitter handle :-",
-                filter: function(val){
-                    return "https://twitter.com/" + val;
-                }
-            },
-            {
-                type: "input",
-                name: "github",
-                message: "Please enter your github handle :-",
-                filter: function(val){
-                    return "https://github.com/" + val;
-                }
+        var questions = [
+                            {
+                                type: "input",
+                                name: "gravatar",
+                                message: "Please enter email address for gravatar link :-",
+                                validate: function( value ) {
+                                    var pass = value.match(/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/);
+                                    if (pass) {
+                                        return true;
+                                    } else {
+                                        return "Please enter link";
+                                    }
+                                },
+                                filter:function(val){
+                                    return "https://s.gravatar.com/avatar/" + MD5(val.trim().toLowerCase());
+                                }
+                            },
+                            {
+                                type: "input",
+                                name: "twitter",
+                                message: "Please enter your twitter handle :-",
+                                filter: function(val){
+                                    return "https://twitter.com/" + val;
+                                }
+                            },
+                            {
+                                type: "input",
+                                name: "github",
+                                message: "Please enter your github handle :-",
+                                filter: function(val){
+                                    return "https://github.com/" + val;
+                                }
 
-            },
-            {
-                type: "input",
-                name: "linkedin",
-                message: "Please enter your linkedin profile link :-",
-                validate: function( value ) {
-                    var pass = value.match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/);
-                    if (pass) {
-                        return true;
-                    } else {
-                        return "Please enter link";
-                    }
-                }
-            }
-        );
+                            },
+                            {
+                                type: "input",
+                                name: "linkedin",
+                                message: "Please enter your linkedin profile link :-",
+                                validate: function( value ) {
+                                    var pass = value.match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/);
+                                    if (pass) {
+                                        return true;
+                                    } else {
+                                        return "Please enter link";
+                                    }
+                                }
+                            }
+                        ];
+        // If Record exists Do something
         if (exists) {
-            // If Record exists Do something
             fs.readFile(outputFilename, 'utf8' ,function (err, data) {
                 var temp = JSON.parse(data);
                 // Prev Record data displayed in question
@@ -121,7 +121,6 @@ inquirer.prompt( start, function( answers ) {
                     });
                 });
             });
-
         }
         //If Record does not exist do something
         else{
